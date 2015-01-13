08eeb28d35a9c073ae97c4c616d6f53a3d9dfa29
Terry Rosen <tcrosen@gmail.com>
2015-01-13 15:23:31 -0500

    fix typo in example

--- example-1-object-reference/1c.js
+++ example-1-object-reference/1c.js
@@ -2,7 +2,7 @@
 // Shows how any method can be overridden (intentionally or not).  Variable scope is important.
 
 console.log = function(message) {
-  console.log('This is not what you expected!');
+  console.info('This is not what you expected!');
 }
 
 console.log('Test log message');
